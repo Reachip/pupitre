cbc27cb5249cdca622e10c1226464bf349667649
Reachip <r.mejri74100@gmail.com>
2019-03-09 20:41:12 +0100

    Ajout d'un système de logs

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 import os
 import json
 import time
+import logging
+from logging.handlers import RotatingFileHandler
 import random
 from pathlib import Path
 
@@ -10,24 +12,49 @@ import RPi.GPIO as GPIO
 from core import event
 from core import leds
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(message)s")
+file_handler = RotatingFileHandler("/home/pi/pupitre.log", "a", 1000000, 1)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+logger.addHandler(stream_handler)
+
+logger.info("le script est initialisé")
+
 event = event.Event()
 event.start()
 
-with open(os.getcwd() + "/pupitre/config.json") as json_file: 
+
+with open("/home/pi/pupitre/config.json") as json_file:
     global json_datas
     json_datas = json.load(json_file)
 
 GPIO.setmode(GPIO.BCM)
-[GPIO.setup(handler, GPIO.OUT, initial=GPIO.HIGH) for handler in json_datas["leds_handlers"]]
+[
+    GPIO.setup(handler, GPIO.OUT, initial=GPIO.HIGH)
+    for handler in json_datas["leds_handlers"]
+]
+
+logger.info("GPIO initialisés")
 
 
 @event.on_with_just_sound
 def with_just_sound():
+    logger.info("demande de jouer le son")
     path = Path(json_datas["music_path"])
     OMXPlayer(path)
+    logger.info("jouer le son fini")
+
 
 @event.on_with_sound_and_leds
 def with_sound_and_led():
+    logger.info("jouer le son et allumer les lumières demandé")
     path = Path(json_datas["music_path"])
     OMXPlayer(path)
 
@@ -40,8 +67,13 @@ def with_sound_and_led():
             GPIO.output(handler, GPIO.HIGH)
             time.sleep(0.1)
 
+    logger.info("jouer le son et allumer les lumières fini")
+
+
 @event.on_with_leds
 def with_leds():
+    logger.info("allumer les lumières")
+
     for loop in range(5):
         for handler in json_datas["leds_handlers"]:
             GPIO.output(handler, GPIO.LOW)
@@ -51,6 +83,7 @@ def with_leds():
             GPIO.output(handler, GPIO.HIGH)
             time.sleep(0.1)
 
+    logger.info("allumer les lumières fini")
+
+
 event.join()
-
-
--- core/event.py
+++ core/event.py
@@ -1,4 +1,6 @@
+import logging
 import threading
+from logging.handlers import RotatingFileHandler
 
 import serial
 import RPi.GPIO as GPIO
@@ -8,14 +10,31 @@ class Event(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.callbacks = {}
-        self.bluetooth_module = serial.Serial(
-            port="/dev/ttyAMA0",
-            baudrate=9600,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            timeout=1,
-        )
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(message)s")
+        file_handler = RotatingFileHandler("/home/pi/pupitre.log", "a", 1000000, 1)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.DEBUG)
+        self.logger.addHandler(stream_handler)
+
+        try:
+            self.bluetooth_module = serial.Serial(
+                port="/dev/ttyAMA0",
+                baudrate=9600,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS,
+                timeout=1,
+            )
+
+        except Exception as error:
+            self.logger.warning("Erreur concernant le serial du Raspberry : " + error)
 
     def on_with_sound_and_leds(self, callback):
         self.callbacks[b"2"] = callback
@@ -32,7 +51,8 @@ class Event(threading.Thread):
     def run(self):
         while True:
             try:
-                self.callbacks[self.bluetooth_module.readline()]()
+                req = self.bluetooth_module.readline()
+                self.callbacks[req]()
 
             except KeyboardInterrupt:
                 GPIO.cleanup()
